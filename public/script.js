5a6bb5ba6a5a6827083dd2d45af399c3af16f0cb
markbyerley622 <markbyerley622@gmail.com>
2025-10-30 08:23:41 +1000

    juice

--- public/script.js
+++ public/script.js
@@ -812,7 +812,7 @@ document.getElementById("dev-w2")?.addEventListener("click", () => updateWinner(
 document.getElementById("dev-w3")?.addEventListener("click", () => updateWinner("High Emperors"));
 
 // === SOLANA POOL TRACKER ===
-const WALLET_ADDRESS = "GzWJccWEybiKvemsDgBec3WuRP2iGQcgKnTG8aNXtFdD";
+const WALLET_ADDRESS = "8JyNWcf5yFWW33zQoUg9sf5f9KgrMkexEYokPcYJYYgj";
 
 // ✅ Your CORS-enabled Helius RPC endpoint
 const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=88759e14-2c77-4ea4-8241-e1f479ac9218";
